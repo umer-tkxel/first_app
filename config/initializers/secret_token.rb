bce59212830b30babeb29888ad01e5dac660b42b
umer.tkxel <umer.riaz@tkxel.com>
2014-03-12 18:14:33 +0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '377dc4b54c91eae9dfedb1153a069b0e4c983201259c970a6cdd1d40470e873e96693add6e99a2651ac9fd1a594fb8666c2d6e1492b8e3744a810ba919d519e5'
